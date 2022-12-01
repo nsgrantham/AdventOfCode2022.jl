b27c01f5fa128c9820ee44f6d876754e020bccd6
Neal Grantham <neal@nsgrantham.com>
2022-12-01 10:34:18 -0800

    Slight speedup

--- src/Day1.jl
+++ src/Day1.jl
@@ -3,19 +3,16 @@ module Day1
 using AdventOfCode2022
 
 function solve(input=pkgdir(AdventOfCode2022, "data", "Day1.txt"))
-    elves = Int[]
-    calories = 0
+    elf_calories = [0]
     for line in eachline(input)
-        if line == ""
-            push!(elves, calories)
-            calories = 0
-        else
-            calories += parse(Int, line)
-        end
+       if line == ""
+           push!(elf_calories, 0)
+       else
+           elf_calories[end] += parse(Int, line)
+       end
     end
-    push!(elves, calories)
-    p1 = maximum(elves)
-    p2 = sum(partialsort!(elves, 1:3, rev=true))
+    p1 = maximum(elf_calories)
+    p2 = sum(partialsort!(elf_calories, 1:3, rev=true))
     p1, p2
 end
 
