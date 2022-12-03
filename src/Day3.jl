891acdcfb07a66e2b21f4ef98f03d6e76be73647
Neal Grantham <neal@nsgrantham.com>
2022-12-03 08:25:13 -0800

    Remove empty lines

--- src/Day3.jl
+++ src/Day3.jl
@@ -11,17 +11,14 @@ priority(item::Char) = islowercase(item) ? item - 'a' + 1 : item - 'A' + 27
 
 function solve(input=pkgdir(AdventOfCode2022, "data", "Day3.txt"))
     rucksacks = readlines(input)
-
     p1 = sum(
         priority(only(intersect(compartments(rucksack)...)))
         for rucksack in rucksacks
     )
-
     p2 = sum(
         priority(only(intersect(group...)))
         for group in Iterators.partition(rucksacks, 3)
     )
-
     p1, p2
 end
 
